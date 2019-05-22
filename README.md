1148ccf42e917361917d39b76ea2a513750a56b4
eviemcn <evie.mcnaughton@gmail.com>
2019-05-22 21:02:06 +1200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# eviemcn.github.io
---
+++ scripts/upAndDownScript.js
@@ -0,0 +1,25 @@
+<script>
+function scrollWin(x, y) {
+  window.scrollBy(x, y);
+}
+</script>
+
+
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../assets/logo1.jpg" class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/logo2.jpg" class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/logo3.jpg" class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/logo4.jpg" class="d-block w-100">
+                    </div>
+                    
+                </div>
+            </div>
